3623173ececbd73c15c20fde5aeeae7303700e09
Daniel Andrade <danfe_1999@outlook.com>
2021-08-23 20:46:12 -0500

    implementation of the isosceles triangle height calculation

--- shapes.js
+++ shapes.js
@@ -16,6 +16,32 @@ function triangleArea (base, height) {
     return (base * height) / 2;
 }
 
+function TriangleHeight (Side1, Side2, Side3){
+    let a,b=0;
+    let res='';
+    if (Side1 == Side2) {
+        a = Side1;
+        b = Side3;
+        res = Math.sqrt((a**2)-((b**2)/4));
+    }
+    else if (Side2 == Side3) {
+        a = Side2;
+        b = Side1;
+        res = Math.sqrt((a**2)-((b**2)/4));
+    }
+    else if (Side3 == Side1) {
+        a = Side3;
+        b = Side2;
+        res = Math.sqrt((a**2)-((b**2)/4));
+    }
+    else{
+        res = 'No es un triangulo Isoceles';
+    }
+
+    return res;
+}
+   
+
 //circle code
 function circleDiameter (radius){
     return radius * 2;
@@ -86,4 +112,17 @@ function CalculateCircleArea() {
 
     const area = circleArea(value);
     alert(area);
+}
+
+function CalculateTriangleHeight (){
+    const input1 = document.getElementById("TriangleIInput1");
+    const input2 = document.getElementById("TriangleIInput2");
+    const input3 = document.getElementById("TriangleIInput3");
+
+    const value1 = parseInt(input1.value);
+    const value2 = parseInt(input2.value);
+    const value3 = parseInt(input3.value);
+
+    const height = TriangleHeight(value1,value2,value3);
+    alert(height);
 }
